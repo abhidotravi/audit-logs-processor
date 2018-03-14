86c5f5e777d59520d01f651d40b145e0a8d2d437
Abhishek Ravi <abhi.ravi@gmail.com>
2018-03-14 16:58:59 -0700

    Fixes for UID to be an INT and not DOUBLE
    - Catch the UID field and read the numeric value as a string.

--- src/main/scala/com/mapr/audit/OpenTSDBPublisher.scala
+++ src/main/scala/com/mapr/audit/OpenTSDBPublisher.scala
@@ -19,18 +19,26 @@ class OpenTSDBPublisher(val host: String, val port: Int) extends Serializable {
       val operation = doc.getString("operation")
       val status = doc.getInt("status")
 
+      var uid: String = "none"
+      try {
+        uid = doc.getInt("uid").toString
+      } catch {
+        case _: Throwable => //do nothing
+      }
+
       val tsdStr = new StringBuilder()
         .append("put ")
         .append("audit.count.metric ")
         .append(timestamp).append(" ")
         .append(1).append(" ")
-        /*.append("operation=").append(operation).append(" ")*/
 
       val docMap: Map[String, AnyRef] = doc.asMap()
 
-      for((key, value) <- docMap.filterKeys(!_.equals("timestamp")))
+      for((key, value) <- docMap.filterKeys(!_.equals("timestamp")).filterKeys(!_.equals("uid")))
         tsdStr.append(key).append("=").append(value.toString).append(" ")
 
+      if(!uid.equals("none"))
+        tsdStr.append("uid=").append(uid)
       writer.println(tsdStr.mkString)
       writer.flush()
       count += 1
